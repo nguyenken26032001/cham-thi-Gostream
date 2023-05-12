c04ce38fd52dedc13c23305dfb8b1f60d3c69c85
Tnig <93302586+Tnig@users.noreply.github.com>
2023-05-12 18:25:15 +0700

    update code

---
+++ src/views/fileUpload.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+<template>
+    <file-upload ref="upload" v-model="files" post-action="/post.method" put-action="/put.method" @input-file="inputFile"
+        @input-filter="inputFilter">
+        Upload file
+    </file-upload>
+</template>
+<style scoped></style>
